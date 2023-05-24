00f4b44c5bd5fed8ee38d8b6cb944f11295dba72
yogeshseroke <99466928+yogeshseroke@users.noreply.github.com>
2023-05-24 15:08:37 +0530

    Add files via upload

---
+++ harshita02_03/OperatorExample.java
@@ -0,0 +1,87 @@
+package harshita02_03;
+
+public class OperatorExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//operator....
+		
+		/* it used to perform operation using one and more operand...
+		 * eg.:- a+b here + is operator and a,b are opearands.
+		 * 
+		 *  type of operators...
+		 *  
+		 *  (1) unary operator :- single operand
+		 *  (2) binary operator :- two operand
+		 *  (3) ternary operator :- three operand
+		 * 
+		 * */
+		
+		
+		//unary operator :- increment(pre, post), decrement(pre, post)
+		
+//		int a=1;
+//		System.out.println(a);
+//		a++;// a = a + 1
+//		System.out.println(a);
+//		++a;//a = a + 1
+//		System.out.println(a);
+		
+		//post-increment => first assign value than increment
+//		int a = 1;
+//		System.out.println("a = "+a);
+//		int b = a++;//b=1
+//		System.out.println("b = "+b);
+//		System.out.println("a = "+a);
+		
+		//pre-increment => first increment than assign value
+		
+//		int a = 1;
+//		System.out.println("a = "+a);
+//		int b = ++a;//b=2
+//		System.out.println("b = "+b);
+//		System.out.println("a = "+a);
+		
+
+		//post-decrement => first assign value than decrease
+		
+//		int a = 1;
+//		System.out.println("a = "+a);//1
+//		int b = a--;//1
+//		System.out.println("b = "+b);
+//		System.out.println("a = "+a);//0
+
+		//pre-decrement => first decrease than assign value
+		
+//		int a = 1;
+//		System.out.println("a = "+a);//1
+//		int b = --a;//0
+//		System.out.println("b = "+b);
+//		System.out.println("a = "+a);//0
+		
+//=================================================
+		
+		//binary operators :- arithmetic, comparison, logical, complex(shorthand)
+		
+		//arithmetic :- +,-,/,*,%
+		
+//		int a=10,b=20,c;
+//		
+//		c = a+b;//30
+//		System.out.println(c);
+//		c = b-a;//10
+//		System.out.println(c);
+//		c = b/a;//2
+//		System.out.println(c);
+//		c = a*b;//200
+//		System.out.println(c);
+//		c = a%b;//10
+//		System.out.println(c);
+
+		//comparison :- a<b, a>b, a==b, a<=b, a>=b, a!=b
+	
+		
+	}
+
+}
