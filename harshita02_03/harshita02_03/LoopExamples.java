081d116d85bea650f942e80849caecdb94c5a5b0
yogeshseroke <99466928+yogeshseroke@users.noreply.github.com>
2023-05-29 15:00:23 +0530

    Add files via upload

---
+++ harshita02_03/harshita02_03/LoopExamples.java
@@ -0,0 +1,135 @@
+package harshita02_03;
+
+public class LoopExamples {
+
+	public static void main(String[] args) {
+		
+//		int a = 1;
+//		int b = 2;
+//		int c = 3;
+//		int d = 4;
+//		int e = 5;
+//		
+//		int x;//delcaration
+//		x=10;//initialization
+		
+		
+//		System.out.println(a);
+//		System.out.println(b);
+//		System.out.println(c);
+//		System.out.println(d);
+//		System.out.println(e);
+		
+		
+		//Loops :- control structure....statement repeated
+		
+		/*
+		 * 		type :- (1)Entry control:-(for, while) (2)Exit control(do-while)
+		 *  
+		 * 	   syntax :- 
+		 * 
+		 * 		(1) for loop :- 
+		 * 
+		 *   				for((1)initialization; (2)condition; (4)iteration)
+		 * 					{
+		 * 
+		 * 						(3)statement....
+		 * 					}
+		 * 							
+		 * ================================================================
+		 * 
+		 * 		(2) while :- 
+		 * 					(1)initialization...
+		 * 
+		 * 					while((2)condition....){
+		 * 							(3)statement...
+		 * 							(4)iteration....
+		 * 						
+		 * 					}
+		 * =================================================================
+		 * 
+		 * 		(3) do-while :-
+		 * 
+		 * 					do{
+		 * 						statement...
+		 * 						iteration...
+		 * 					}while(condition....);
+		 * */
+		
+		//forward direction....
+//		for(int i=1; i<=10; i++) {
+//			System.out.println(i);
+//		}
+		
+		//backward direction....
+//		for(int i=10; i>=1; i--) {
+//			System.out.println(i);
+//		}
+		
+		//table of 2...
+//		int num = 2;
+//		for(int i=1; i<=10; i++) {
+//			System.out.println(num*i);
+//		}
+		
+//		int num = 5;
+//		for(int i=10; i>=1; i--) {
+//			System.out.println(num*i);
+//		}
+		
+		//prime number.... 5/1, 5/5
+		
+//		int num = 11;
+//		int count = 0;
+//		for(int i=1; i<=num; i++) {
+//			if(num%i==0) {//6/1=0, 6/2=0, 6/3=0, 6/6=0
+//					count++;//1, 2, 3, 4
+//			}
+//		}
+//		if(count==2) {
+//			System.out.println("prime");
+//		}else {
+//			System.out.println("not prime");
+//		}
+		
+		//factorial... 5 = 5*4*3*2*1 = 120
+		
+//		int num = 5;
+//		
+//		int fact=num;//5
+//		
+//		for(int i=1; i<fact; i++) {
+//			num =  (num * i);//5, 10, 30, 120
+//		}
+//		
+//		System.out.println(num);
+		
+		//fibonacci series... 0 1 1 2 3 5 8 21
+		/*
+		 *      0 1 1 2 3 5
+		 *        
+		 *      a b = c
+		 *		0 1 = 1
+		 *      1 1 = 2
+		 *      1 2 = 3
+		 *      2 3 = 5
+		 *      3 5 = 8
+		 *      5 8 = 13     
+ 		 * 
+		 * */
+		
+//		int a=0,b=1,c=0;
+//		
+//		for(int i=1; i<8; i++) {
+//			System.out.print(a+" ");
+//			c = a + b;//1
+//			a = b;//1
+//			b = c;//1
+// 		}
+		
+		//armStrong number... 153 = (1)3 + (5)3 + (3)3 = 1 + 125 + 27 = 153
+		
+		
+	}
+
+}
