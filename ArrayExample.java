8950c6f7ba5144564186cc60cc71fe5861155a5a
yogeshseroke <99466928+yogeshseroke@users.noreply.github.com>
2023-06-05 15:02:04 +0530

    Add files via upload

---
+++ ArrayExample.java
@@ -0,0 +1,148 @@
+package harshita02_03;
+
+public class ArrayExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//before array...
+		
+//		int a = 1;
+//		int b = 2;
+//		int c = 3;
+//		int d = 4;
+//		int e = 5;
+		
+		//array... similar type ke elements store....
+		
+		/*
+		 * 		
+		 * 
+		 *   syntax :- 
+		 *   
+		 *   (1) datatype variable[] = new datatype[size];
+		 *   	 int ar[] = new int[5];
+		 *       int[] ar = new int[5];
+		 *   
+		 *   (2) datatype variable[] = {v1, v2, v3,....};
+		 * 		 int ar[] = {1,2,3,4,5};
+		 * 					 0 1 2 3 4 = size	
+				 
+				 int[] ar = {2,9,3,8,4};	
+		 * 		
+		 * 		length = 5;
+		 * 
+		 * 			ar[0] = 1;
+		 * 			ar[1] = 2;	
+		 * 		
+		 * 	size of array = (length - 1) 
+		 *            size=4
+		 *            
+		 *   Limitations..... (1) size fixed (2) similar type elements
+		 *            
+		 *   Types of array :- (single dimensional, multidimensional, jagged array)         
+		 *            
+		 * */
+		
+//		int ar[] = {1,2,3,4,5}; 
+//		
+//		for(int i=0; i<=ar.length-1; i++) {
+//			System.out.print(ar[i]+" ");
+//		}
+		
+//		int ar[] = new int[5];
+//		ar[0] = 10;
+//		ar[1] = 20;
+//		ar[2] = 30;
+//		ar[3] = 40;
+//		ar[4] = 50;
+		
+//		for(int i=0; i<=ar.length-1; i++) {
+//			System.out.print(ar[i]+" ");
+//		}
+		
+//		String ar[] = {"C","C++","Java","python"};
+//		
+//		for(int i=0; i<=ar.length-1; i++) {
+//			System.out.println(ar[i]);
+//		}
+		
+//		char ar[] = {'a','x','b','y'};
+//		for(int i=0; i<=ar.length-1; i++) {
+//			System.out.println(ar[i]);
+//		}
+		
+//		int ar[] = {1,2,9,3,7,4};
+//		System.out.println(ar[2]);
+		
+//		Object ar[] = {12, 1.2f, 'a', "XYZ", 12.34, true};
+//		
+//		for(int i=0; i<=ar.length-1; i++) {
+//			System.out.println(ar[i]);
+//		}
+		
+		//coding questions....
+		
+		//ascending, descending, maximum, secondMaximum, smallest
+		
+		//ascending... 
+//		int ar[] = {1,9,2,8,5,7,4,6,3};
+//		int temp;            
+//		
+//		for(int i=0; i<=ar.length-1; i++) {
+//			System.out.print(ar[i]+" ");
+//		}
+		
+//		for(int i=0; i<=ar.length-1; i++) {
+//			for(int j=0; j<=ar.length-1; j++) {
+//				if(ar[i]<ar[j]) {         //ar[0] < ar[0]
+//					temp = ar[i];//1
+//					ar[i] = ar[j];//9
+//					ar[j] = temp;//1
+//				}
+//			}
+//			
+//		}
+		/*
+		 * ar[0] < ar[1]
+		 * 1<9
+		 * 
+		 * */
+		
+//		System.out.println();
+//		
+//		for(int i=0; i<=ar.length-1; i++) {
+//			System.out.print(ar[i]+" ");
+//		}
+		
+		//descending....
+		//int ar[] = {1,9,2,8,4,7,5,6,3};
+		
+		//9,8,7,6,5,4,3,2,1
+		
+//		int temp;
+//		
+//		for(int i=0; i<=ar.length-1; i++) {
+//			System.out.print(ar[i]+" ");
+//		}
+//		
+//		for(int i=0; i<=ar.length-1; i++) {
+//			for(int j=0; j<=ar.length-1; j++) {
+//				if(ar[i]>ar[j]) {
+//					temp=ar[i];
+//					ar[i]=ar[j];
+//					ar[j]=temp;
+//				}
+//			}
+//		}
+//		
+//		System.out.println();
+//		
+//		for(int i=0; i<=ar.length-1; i++) {
+//			System.out.print(ar[i]+" ");
+//		}
+		
+		
+	}
+
+}
